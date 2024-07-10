6580154836b22519191f6d8aaa4d031140799e95
nkosi <nkosi.cele@simplify.africa>
2024-07-10 19:04:32 +0200

    completed conftest

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,18 +1,25 @@
 import time
 
+import pytest
 from selenium import webdriver
 
 
-def setup(browser):
-    if browser.lower() or browser.upper() == 'chrome':
+@pytest.fixture()
+def setup(browser,url):
+    if browser.lower() == 'chrome':
         driver = webdriver.Chrome()
-        time.sleep(2)
 
-    elif browser.lower() or browser.upper() == 'firefox':
+    elif browser.lower() == 'firefox':
         driver = webdriver.Firefox()
     else:
         driver = webdriver.Edge()
+    return driver
 
-    return  driver
 
-setup('chrome')
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
