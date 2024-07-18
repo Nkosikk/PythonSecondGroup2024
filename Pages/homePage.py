9d068165b23aed4edd1850b3d979c56e23fb44e8
zwaid <zwaidweba03@gmail.com>
2024-07-18 12:37:53 +0200

    Created add back pack to cart

--- Pages/homePage.py
+++ Pages/homePage.py
@@ -4,14 +4,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class HomePage:
-    burgermenu = "react-burger-menu-btn"
+    burgerMenu_id = "react-burger-menu-btn"
     addBackPack_id = "add-to-cart-sauce-labs-backpack"
 
     def __init__(self, driver):
         self.driver = driver
     def verifyBurgerMenu(self):
         wait = WebDriverWait(self.driver, 15)
-        element = wait.until(EC.element_to_be_clickable((By.ID, self.burgermenu)))
+        element = wait.until(EC.element_to_be_clickable((By.ID, self.burgerMenu_id)))
         element.is_displayed()
     def selectBackPack(self):
         wait = WebDriverWait(self.driver, 15)
